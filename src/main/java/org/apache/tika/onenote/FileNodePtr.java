573ca79d4bbe9416504982fb9bf6f8f9e8e34641
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2019-12-04 06:04:41 -0600

    more javadoc

--- src/main/java/org/apache/tika/onenote/FileNodePtr.java
+++ src/main/java/org/apache/tika/onenote/FileNodePtr.java
@@ -5,13 +5,6 @@ import java.util.List;
 
 /**
  * Stores a list that represents how to get to the file node in the data structure.
- * First element of the nodeListPositions is the index of the FileNode at the root.
- * Next element of the nodeListPosition is the index at the next level.
- *
- * For example 0, 4, 15 would mean
- *
- * document.root.children.get(0).childFileNodeList.children.get(4).childFileNodeList.children.get(15)
- *
  */
 public class FileNodePtr {
   List<Integer> nodeListPositions = new ArrayList<>();
@@ -24,6 +17,22 @@ public class FileNodePtr {
     nodeListPositions.addAll(copyFrom.nodeListPositions);
   }
 
+  /**
+   * Uses the nodeListPositions to get the FileNode from the document.root hierarchy.
+   * <p>
+   * It works like this:
+   * <p>
+   * The first element of the nodeListPositions is the index of the FileNode at the root.
+   * The next element of the nodeListPosition is the index at the child of the first element.
+   * And so on...
+   * <p>
+   * For example 0, 4, 15 would mean
+   * <p>
+   * document.root.children.get(0).childFileNodeList.children.get(4).childFileNodeList.children.get(15)
+   *
+   * @param document
+   * @return
+   */
   public FileNode dereference(OneNoteDocument document) {
     if (nodeListPositions.isEmpty()) {
       return null;
